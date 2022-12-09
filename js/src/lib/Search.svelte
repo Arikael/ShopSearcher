828eaf5c1f62114adf0e26a1dac5688fa71264de
Arikael <mhaeberle@mailbox.org>
2022-12-09 10:35:18 +0100

    move to go subdir

---
+++ js/src/lib/Search.svelte
@@ -0,0 +1,13 @@
+<script>
+     fetch('configs/swiss_wine.json').then(x => console.log(x))
+
+
+    const search = () => {
+
+    }
+</script>
+
+<input type="text">
+<button on:click={search}>
+    Search
+</button>
