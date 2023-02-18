40dbd659d9abbbbf108b2e34fbb7faa2dda25e45
Arikael <mhaeberle@mailbox.org>
2023-02-18 17:31:02 +0100

    add link to firefox addon

--- js/src/lib/MobileInfo.svelte
+++ js/src/lib/MobileInfo.svelte
@@ -2,6 +2,7 @@
     <strong>It seems you are using a mobile browser<br/>
         Unfortunately the search probably won't work correctly<br/>
         Browser prevent (rightfully so) opening multiple tabs/windows programmatically<br/>and
-        while this can be allowed on a per page base on desktop it seems mobile browser always prevent it
+        while this can be allowed on a per page base on desktop it seems mobile browser always prevent it<br />
+        you can install <a href="https://addons.mozilla.org/en-GB/firefox/addon/shopsearcher/">the firefox extension</a>
     </strong>
 </div>
