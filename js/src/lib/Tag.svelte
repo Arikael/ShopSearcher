de0db8128296ea0abb0ae1147b89be52f605acdf
Arikael <mhaeberle@mailbox.org>
2023-02-21 21:04:30 +0100

    improve tag colors

--- js/src/lib/Tag.svelte
+++ js/src/lib/Tag.svelte
@@ -1,12 +1,37 @@
+<style>
+    .tag {
+        padding: 4px 8px;
+        background: rebeccapurple;
+        border-radius: var(--default-border-radius);
+        margin: 0 4px;
+    }
+
+    .tag-clickable:hover {
+        cursor: pointer;
+        background: #8e76a7;
+    }
+
+    .tag.tag-selected {
+        background: #a05389;
+    }
+
+    @media (prefers-color-scheme: light) {
+        .tag {
+            background: #744d9f;
+            color: #fff;
+        }
+    }
+</style>
 <script lang="ts">
     import {createEventDispatcher} from 'svelte';
 
     export let isClickable = false
     export let tag = ''
+    export let isSelected = false
     const dispatch = createEventDispatcher()
 
     function toggleTag(tag: string) {
-        if(!isClickable) {
+        if (!isClickable) {
             return
         }
 
@@ -16,6 +41,6 @@
     }
 </script>
 
-<span class:tag-clickable="{isClickable}" class="tag" on:click={() => toggleTag(tag)}>
+<span class:tag-clickable="{isClickable}" class:tag-selected="{isSelected}" class="tag" on:click={() => toggleTag(tag)}>
     {tag}
 </span>
